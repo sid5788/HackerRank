44ed163ae7f4ab84eecbd4ec67f86388f00f5b42
sid5788 <sid5788@gmail.com>
2016-02-02 04:22:21 -0500

    Fixed Right Side limit
    Execution is still slow . Needs improvement in execution time for large
    values.

--- src/maxXOR/Solution.java
+++ src/maxXOR/Solution.java
@@ -1,5 +1,5 @@
 package maxXOR;
-
+//Sid: Test Case 10 -> Timed Out
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -11,23 +11,21 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int Q = in.nextInt();
-        long x,result,first;
+        long x,result;
         for(int a0 = 0; a0 < Q; a0++){
             long L = in.nextLong();
             long R = in.nextLong();
         
         x =0; 
         result = 0;
-        first =0;
-        for(long i = 0; i <R; i++){
+        for(long i = 0; i <=R; i++){
             x = x^i;
             if (i >=L){
-                if (first == 0){
-                    first = 1;
+                if (i == L){
                     result = x;
                 }else{
                 result = result^x;
-            }
+                }
             }
         }
         System.out.println(result);
